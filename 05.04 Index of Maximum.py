0f69ca92246ccdb5a46c628edda30154c0a2adca
parikshya098 <pbhandari1@ualr.edu>
2022-02-16 02:24:56 +0000

    Unit 5

--- 05.04 Index of Maximum.py
+++ 05.04 Index of Maximum.py
@@ -1,11 +1,3 @@
-# max = 0
-# a = int(input('Enter a Number (zero to quit): '))
-# while a != 0:
-#     if a > max:
-#         max = a
-#     a = int(input('Enter a Number (zero to quit): '))
-# print('Maximum:',max)
-
 x = int(input("Enter a number(zero to quit): "))
 maximum = x
 maximumindex = 1
